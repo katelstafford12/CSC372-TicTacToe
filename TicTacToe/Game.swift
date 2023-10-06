8cc1978dc70ebf8d08188144b774e7781b32dfd0
Shivugoyal2210 <shivugoyal22@gmail.com>
2023-10-06 11:08:03 -0700

    Update Game.swift
    Added check win and a random AI

--- TicTacToe/Game.swift
+++ TicTacToe/Game.swift
@@ -40,6 +40,7 @@ class Game: ObservableObject{
         } else {
             current_board[row][column].mark = Mark.O
         }
+        checkWin()
         
         // For changing turns
         if turn == Mark.X {
@@ -47,7 +48,75 @@ class Game: ObservableObject{
         } else {
             turn = Mark.X
         }
-        
-        //TODO: implement a check if anyone has won
+    }
+
+    func AIturn(){
+        var availablePositions = [(row: Int, column: Int)]()
+
+    // Find all available positions on the board
+    for row in 0...2 {
+        for column in 0...2 {
+            if current_board[row][column].mark == Mark.free {
+                availablePositions.append((row, column))
+            }
+        }
+    }
+
+    // Check if there are available positions to make a move
+    if availablePositions.isEmpty {
+        return 
+    }
+
+    // Randomly select one available position
+    let randomIndex = Int.random(in: 0..<availablePositions.count)
+    let (row, column) = availablePositions[randomIndex]
+
+    // Place the AI's mark in the selected position
+    current_board[row][column].mark = turn
+    checkWin() // Check for a win after making the move
+
+    // Switch the turn to the player's turn
+    if turn == Mark.X {
+        turn = Mark.O
+    } else {
+        turn = Mark.X
+    }
+    }
+
+    func checkWin(){
+        // Check rows for a win
+    for row in current_board {
+        if row.allSatisfy { $0.mark == Mark.X } {
+            // Player X has won
+            return
+        } else if row.allSatisfy { $0.mark == Mark.O } {
+            // Player O has won
+            return
+        }
+    }
+
+    // Check columns for a win
+    for columnIndex in 0...2 {
+        let column = current_board.map { $0[columnIndex] }
+        if column.allSatisfy { $0.mark == Mark.X } {
+            // Player X has won
+            return
+        } else if column.allSatisfy { $0.mark == Mark.O } {
+            // Player O has won
+            return
+        }
+    }
+
+    // Check diagonals for a win
+    let diagonal1 = [current_board[0][0], current_board[1][1], current_board[2][2]]
+    let diagonal2 = [current_board[0][2], current_board[1][1], current_board[2][0]]
+
+    if diagonal1.allSatisfy { $0.mark == Mark.X } || diagonal2.allSatisfy { $0.mark == Mark.X } {
+        // Player X has won
+        return
+    } else if diagonal1.allSatisfy { $0.mark == Mark.O } || diagonal2.allSatisfy { $0.mark == Mark.O } {
+        // Player O has won
+        return
+    }
     }
 }
