1ac1428493cfcda9b646508b3ac4c4831f3c8de2
Shivugoyal2210 <shivugoyal22@gmail.com>
2023-10-09 22:15:34 -0700

    added testcases

--- TicTacToeTests/TicTacToeTests.swift
+++ TicTacToeTests/TicTacToeTests.swift
@@ -9,23 +9,60 @@ import XCTest
 @testable import TicTacToe
 
 final class TicTacToeTests: XCTestCase {
+    var game: TicTacToe.Game!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        var game = TicTacToe.Game();
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        game = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testIsNotDrawWhenThereIsAWinner() {
+        // Simulate a game where there's a winner
+        // You should set up the game board accordingly based on your game's implementation
+
+        game.putMark(0, 0)
+        game.putMark(1, 1)
+        game.putMark(0, 1)
+        game.putMark(2, 2)
+        game.putMark(0, 2) 
+
+        XCTAssertFalse(game.isDraw())
     }
 
+    func testHasWonHorizontal() {
+    // Simulate a game where a player has won horizontally
+        game.putMark(0, 0)
+        game.putMark(1, 1)
+        game.putMark(0, 1)
+        game.putMark(2, 2)
+        game.putMark(0, 2) 
+
+    // Check if the game is considered as won
+    XCTAssertTrue(game.hasWon())
+}
+
+    func testHasWonVertical() {
+    game.setMark(row: 0, col: 0, mark: .o)
+    game.setMark(row: 1, col: 0, mark: .o)
+    game.setMark(row: 2, col: 0, mark: .o)
+    game.putMark(0, 0)
+        game.putMark(1, 1)
+        game.putMark(1, 0)
+        game.putMark(2, 2)
+        game.putMark(2, 0) 
+    // Check if the game is considered as won
+    XCTAssertTrue(game.hasWon())
+}
+
+func testHasNotWon() {
+    game.setMark(0,0)
+    game.setMark(1,0)
+    XCTAssertFalse(game.hasWon())
+}
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
